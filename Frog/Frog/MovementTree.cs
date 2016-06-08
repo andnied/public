a3bc0fe4bde731e42349a5222278bbacb4eba5c9
Andrzej Niedzielan <andnied@outlook.com>
2016-06-08 09:45:17 +0200

    add Frog solution

---
+++ Frog/Frog/MovementTree.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frog
+{
+    public class MovementTree
+    {
+        private readonly int _number;
+
+        public MovementTree(int number)
+        {
+            _number = number;
+        }
+
+        public MovementTree Left { get; set; }
+        public MovementTree Right { get; set; }
+
+        public void FillNodes()
+        {
+            if ((_number - 1) >= 0)
+            {
+                Left = new MovementTree(_number - 1);
+                Left.FillNodes();
+            }
+            
+            if ((_number - 2) >= 0)
+            {
+                Right = new MovementTree(_number - 2);
+                Right.FillNodes();
+            }
+        }
+
+        public int CountPaths()
+        {
+            int sum = 0;
+
+            if (Left == null && Right == null)
+                return 1;
+            else
+            {
+                if (Left != null)
+                    sum = Left.CountPaths();
+                if (Right != null)
+                    sum += Right.CountPaths();
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Frog/Frog/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inches = 0;
+            string tryAgain = string.Empty;
+
+            do
+            {
+                Console.WriteLine("Please provide the number of inches.");
+                var read = Console.ReadLine();
+
+                if (int.TryParse(read, out inches))
+                {
+                    if (inches < 1)
+                        Console.WriteLine("The number of inches should be greater than 0.");
+                    else
+                    {
+                        var tree = new MovementTree(inches);
+                        tree.FillNodes();
+                        var result = tree.CountPaths();
+
+                        Console.WriteLine(string.Format("There are {0} possibilities.", result));
+                    }
+                }
+                else
+                    Console.WriteLine("Incorrect argument. Should be a number.");
+
+                Console.WriteLine("Want to try again? (y/n)");
+                tryAgain = Console.ReadLine();
+            } while (tryAgain == "y");
+        }
+    }
+}
